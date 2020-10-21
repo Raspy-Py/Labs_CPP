feb614bd43526ba1c226a3f2f37967babb349026
naumenko.roman2003 <naumenko.roman2003@gmail.com>
2020-10-21 11:38:39 +0300

    Добавьте файлы проекта.

---
+++ Lab1_v27/Lab1_v27.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	float c; // Гіпотенуза
+	float a; // Перший катет
+	float b; // Другий катет
+	float r; // Радіус вписаного кола
+
+	std::cout << "Enter hypotenuse: ";
+	std::cin >> c;
+
+	std::cout << "Enter first leg: ";
+	std::cin >> a;
+
+	if (c <= a || (c <= 0 || a <= 0))
+	{
+		std::cout << "This triangle doesn't exist" << std::endl;
+	}
+	else
+	{
+		b = pow((c * c - a * a), 0.5);
+		r = (a + b - c) / 2;
+		std::cout << "The radius of the inscribed circle: " << r << std::endl;
+	}
+}
