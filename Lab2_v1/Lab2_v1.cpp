1d2b91b83145eff4f67e6cfea9713d8cfedba7ef
naumenko.roman2003 <naumenko.roman2003@gmail.com>
2020-10-28 16:46:00 +0200

    Laba_2

---
+++ Lab2_v1/Lab2_v1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float X, Y;
+
+    cout << "Enter X: ";
+    cin >> X;
+
+    cout << "Entet Y: ";
+    cin >> Y;
+
+    if (Y > 1 - X)
+    {
+        if (X <= 0 || Y <= 0)
+        {
+            cout << "The point belongs to the shaded area";
+        }
+        else
+        {
+            cout << "The point does not belong to the shaded area";
+        }
+    }
+    else
+    {
+        cout << "The point does not belong to the shaded area";
+    }
+}
