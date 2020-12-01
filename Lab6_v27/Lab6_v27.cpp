a85673ea32d7e4ad061a8e3f89bab877a29634bd
naumenko.roman2003 <naumenko.roman2003@gmail.com>
2020-12-01 22:08:23 +0200

    +Lab_6

---
+++ Lab6_v27/Lab6_v27.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+float min(float a, float b);
+
+int main()
+{
+    float PI = 3.141592;
+    float a, b; // Enter data
+    float U, V, W; // Res
+
+    cout << "Enter a:";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+
+    U = min(a, b);
+    V = min(a * b, a + b);
+    W = min(U + pow(V, 2), PI);
+
+    cout << "U = " << U << "\nV = " << V << "\nW = " << W << endl;
+}
+
+float min(float a, float b)
+{
+    return a <= b ? a : b;
+}
